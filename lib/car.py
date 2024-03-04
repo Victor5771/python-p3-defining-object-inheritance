4de037065c55b75c69c1d2ab098a9c13cb4f67ac
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-03-04 18:09:19 +0300

    Test passed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number, brand="", model=""):
+        super().__init__(wheel_size, wheel_number)
+        self.brand = brand
+        self.model = model
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def honk(self):
+        return "Beep beep!"
